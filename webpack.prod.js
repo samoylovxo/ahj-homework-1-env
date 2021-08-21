a303e815b71c0531c471a8672a4db1f66cd4b0de
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-08-21 17:45:37 +0300

    webpack configs edited

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,13 @@
+/* eslint-disable linebreak-style */
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-// const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimize: true,
-    minimizer: [
-      new OptimizeCSSAssetsPlugin({}),
-      '...',
-    ],
+    minimizer: [new OptimizeCSSAssetsPlugin({}), new TerserPlugin({})],
   },
 });
